bfad73656c4889804a49d33ea8237381ccc896ea
rizalfr <zacksupreme@gmail.com>
2026-07-21 17:05:35 +0700

    Add thumb scroll mode to mouse layer

--- keyboards/keyball/keyball39/keymaps/manna-harbour_miryoku/config.h
+++ keyboards/keyball/keyball39/keymaps/manna-harbour_miryoku/config.h
@@ -65,7 +65,7 @@ LAYOUT_miryoku( \
     K00, K01, K02, K03, K04,        K05, K06, K07, K08, K09, \
     K10, K11, K12, K13, K14,        K15, K16, K17, K18, K19, \
     K20, K21, K22, K23, K24,        K25, K26, K27, K28, K29, \
-    N30, N31, K32, K33, K34,        K35, K36, K37, N38, N39 \
+    N30, N31, K32, K33, K34,        KC_BTN2, KC_BTN1, SCRL_MO, N38, N39 \
 )
 
 #define MIRYOKU_LAYERMAPPING_BASE LAYOUT_keyball39_base
